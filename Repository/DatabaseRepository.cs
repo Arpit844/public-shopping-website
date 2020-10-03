df0311a81d59dd308b502accd0ba77b0d999adb9
Arpit844 <72178637+Arpit844@users.noreply.github.com>
2020-10-03 08:39:29 +0530

    Update DatabaseRepository.cs

--- Repository/DatabaseRepository.cs
+++ Repository/DatabaseRepository.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.Linq;
+\\type your all text information
 using System.Text;
 using System.Threading.Tasks;
 
